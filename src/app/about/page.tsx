bf72f5780cd2e7455f30fc2a0b3af750de154a8b
Prathama Shrestha <prathamastha7@gmail.com>
2025-03-20 19:51:42 +0545

    main pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,20 @@
+import About from "./_sections/About";
+import Info from "./_sections/Info";
+import Value from "./_sections/Value";
+import Values from "./_sections/Values";
+import Member from "./_sections/Member";
+import Members from "./_sections/Members";
+
+
+export default function Home() {
+  return(
+    <div>
+      <About />
+      <Info />
+      <Value />
+      <Values />
+      <Member />
+      <Members />
+    </div>
+  );
+}
