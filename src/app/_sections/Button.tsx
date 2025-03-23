ea3a1159f3edfa8a254bf996ce500b2379921266
shakti-kc <shaktikc63@gmail.com>
2025-03-23 12:14:31 +0545

    changes to button and about page

--- src/app/_sections/Button.tsx
+++ src/app/_sections/Button.tsx
@@ -15,16 +15,16 @@ function Button() {
         <path
           d="M20.0001 11.9998L4.00012 11.9998"
           stroke="#fefefe"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M15.0003 17C15.0003 17 20.0002 13.3176 20.0002 12C20.0002 10.6824 15.0002 7 15.0002 7"
           stroke="#fefefe"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </div>
