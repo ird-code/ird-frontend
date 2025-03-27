4f916e1a91f0b9673003671e720a8019b3629d5c
Prathama Shrestha <prathamastha7@gmail.com>
2025-03-27 15:32:03 +0545

    fix: made news link responsive

--- src/app/news/_sections/Partners.tsx
+++ src/app/news/_sections/Partners.tsx
@@ -4,15 +4,18 @@ import Button from "./Button";
 
 function Partners() {
   return (
-    <div className='w-full bg-[#eeeeee] py-15'>
+    <div className="w-full bg-[#eeeeee] py-15">
       <div className="p-7 flex flex-col justify-center items-center gap-4">
         <h2 className="text-3xl font-semibold text-center">Our Partners</h2>
       </div>
 
-      <div className='w-full flex justify-center'>
-        <div className='w-full flex gap-10 px-10 py-10 justify-center items-center max-w-[1440px] flex-wrap'>
+      <div className="w-full flex justify-center">
+        <div className="w-full flex gap-10 px-10 py-10 justify-center items-center max-w-[1440px] flex-wrap">
           {[...Array(5)].map((_, index) => (
-            <div key={index} className='w-32 h-32 md:w-40 md:h-40 relative grayscale hover:grayscale-0'>
+            <div
+              key={index}
+              className="w-32 h-32 md:w-40 md:h-40 relative grayscale hover:grayscale-0"
+            >
               <Image
                 src="/assets/images/logo2.png"
                 alt="Partner Image"
@@ -25,15 +28,16 @@ function Partners() {
         </div>
       </div>
 
-      <div className='w-full flex justify-center'>
-        <div className='w-full flex flex-col md:flex-row gap-10 px-10 py-10 my-10 justify-between max-w-[1440px]'>
-          <div className='md:w-2/3'>
+      <div className="w-full flex justify-center">
+        <div className="w-full flex flex-col md:flex-row gap-10 px-10 py-10 my-10 justify-between max-w-[1440px]">
+          <div className="md:w-2/3">
             <h2 className="text-2xl font-semibold text-left text-[#850d09]">
-              We collaborate with dedicated partners to drive sustainable rural development and create lasting impact
+              We collaborate with dedicated partners to drive sustainable rural
+              development and create lasting impact
             </h2>
           </div>
-          <div className='md:w-1/3 flex justify-end'>
-            <Button/>
+          <div className="md:w-1/3 flex justify-end">
+            <Button />
           </div>
         </div>
       </div>
