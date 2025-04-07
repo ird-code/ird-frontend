a6c40af9a5b5acc80fd21adcfa3c14b2f0989a1d
shakti-kc <shaktikc63@gmail.com>
2025-04-08 00:21:33 +0545

    changed to about-us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,4 @@
 import About from "./_sections/About";
-import Info from "./_sections/Info";
-import Value from "./_sections/Value";
 import Values from "./_sections/Values";
 import Member from "./_sections/Member";
 import Members from "./_sections/Members";
@@ -10,8 +8,6 @@ export default function Home() {
   return(
     <div>
       <About />
-      <Info />
-      <Value />
       <Values />
       <Member />
       <Members />
