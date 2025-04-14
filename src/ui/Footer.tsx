83822186bc825600757d0abea5d471c13d9f614e
shakti-kc <shaktikc63@gmail.com>
2025-04-14 12:04:53 +0545

    update footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
   return (
     <footer className="flex w-full bg-[#850d09] justify-center items-center p-20 py-[5rem] max-md:p-4 max-md:py-8">
       <div className="w-full max-w-[1440px]">
-        <div className="w-full flex max-md:flex-col max-md:items-center max-md:gap-8 justify-between px-10 max-md:px-4">
-          <div className="flex items-center gap-5 max-md:flex-wrap max-md:justify-center">
+        <div className="w-full flex max-md:flex-col max-md:gap-8 justify-between px-10 max-md:px-4">
+          <div className="flex items-center gap-5 max-md:flex-wrap">
             <p className="text-2xl font-semibold text-[#fefefe] max-md:text-xl">
               Follow Us
             </p>
@@ -30,7 +30,7 @@ const Footer = () => {
             </Link>
           </div>
 
-          <div className="flex items-center gap-3 max-md:flex-col">
+          <div className="flex items-center gap-3">
             <AiOutlineGlobal className="text-2xl font-semibold !text-[#fefefe] max-md:text-xl" />
             <p className="text-2xl font-semibold text-[#fefefe] max-md:text-xl">
               NP
@@ -49,22 +49,22 @@ const Footer = () => {
             <div className="flex flex-col gap-2">
               {/* ... area items */}
               <ul>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe] pb-2">
                   <Link href="/about"> About</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/events"> Events</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/news"> News</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/involve"> Involve</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/gallery"> Gallery</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="#"> Donate</Link>
                 </li>
               </ul>
@@ -78,15 +78,15 @@ const Footer = () => {
             </h3>
             <div className="flex flex-col gap-2">
               <ul>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/areas-of-work/agriculture">Agriculture</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/areas-of-work/culture-and-heritage">
                     Culture & Heritage
                   </Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/areas-of-work/tourism">Tourism</Link>
                 </li>
               </ul>
@@ -100,25 +100,25 @@ const Footer = () => {
             </h3>
             <div className="flex flex-col gap-2">
               <ul>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/initiative/education-reform">
                     Education Reform
                   </Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/initiative/ex-l">EXL</Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/initiative/literacy-course-package">
                     Literacy Course Package
                   </Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/initiative/local-curriculum-development">
                     Local Curriculum Development
                   </Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/initiative/maitri">Maitri</Link>
                 </li>
               </ul>
@@ -132,12 +132,12 @@ const Footer = () => {
             </h3>
             <div className="flex flex-col gap-2">
               <ul>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/flagship-programs/conscious-village-initiative">
                     Conscious Village
                   </Link>
                 </li>
-                <li className="!text-[1.2rem] !text-[#fefefe] font-medium pb-2">
+                <li className="!text-[1.2rem] !text-[#fefefe]  pb-2">
                   <Link href="/flagship-programs/one-app-one-tap">
                     One App One Tap
                   </Link>
@@ -147,7 +147,7 @@ const Footer = () => {
           </div>
 
           {/* Logo Column */}
-          <div className="h-full flex flex-col gap-5 max-md:items-center">
+          <div className="h-full flex flex-col gap-5 max-md:w-full">
             <div className="bg-[#fefefe] flex justify-center items-center p-2 rounded-3xl max-md:w-40">
               <Image
                 src="/assets/images/logo3.png"
@@ -157,22 +157,22 @@ const Footer = () => {
                 className="max-md:w-full max-md:h-auto"
               />
             </div>
-            <div className="flex flex-col gap-2 items-end max-md:items-center">
-              <p className="text-lg text-[#fefefe] max-md:text-base max-md:text-center">
+            <div className="flex flex-col gap-2 items-end max-md:items-start max-md:w-full">
+              <p className="text-lg text-[#fefefe] text-end max-md:text-start">
                 Panchakanya-1, Nuwakot, NP
-              </p>{" "}
-              <p className="text-lg text-[#fefefe] max-md:text-base max-md:text-center">
+              </p>
+              <p className="text-lg text-[#fefefe] text-end max-md:text-start">
                 +977 9843504464
               </p>
-              <p className="text-lg text-[#fefefe] max-md:text-base max-md:text-center">
+              <p className="text-lg text-[#fefefe] text-end max-md:text-start">
                 contact@ird.com.np
               </p>
             </div>
           </div>
         </div>
 
-        <div className="w-full p-5 flex justify-center max-md:p-2">
-          <p className="text-[#fefefe] text-lg max-md:text-sm max-md:text-center">
+        <div className="w-full flex justify-center max-md:p-2">
+          <p className="text-[#fefefe] text-lg max-md:text-sm">
             Copyright 2025 Institute For Rural Development
           </p>
         </div>
